e5650b1c49d80fd055fbed8d4fdef6f2fb54cf8a
adoumasseo <adoumasseo@gmail.com>
2024-11-13 09:48:22 +0100

    Concat two string and type annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" For concat two string and learn types annotations """
+
+
+def concat(str1: str, str2: str) -> str:
+    """For concat two string"""
+    return str1 + str2
