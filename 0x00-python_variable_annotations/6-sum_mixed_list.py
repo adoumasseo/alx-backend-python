77af2df25a214baee9e3396779832ee9fd93c23f
adoumasseo <adoumasseo@gmail.com>
2024-11-13 13:00:43 +0100

    fix: make the file executable, add doc for the function, and use Union instead of |

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """6's module doc"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """sum but with notation"""
     return float(sum(mxd_lst))
