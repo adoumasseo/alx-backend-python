cf56994d1423f56fbcff9c817f515d02cf0ff017
adoumasseo <adoumasseo@gmail.com>
2024-11-13 12:30:59 +0100

    Sum but with annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Sum of list element but with annotations"""
+
+
+from ast import For
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Take a list and return the sum of his elt"""
+    sum: float = 0
+    for item in input_list:
+        sum += item
+    return sum
