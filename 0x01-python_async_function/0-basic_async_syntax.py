631b84fa1a0ea96a0406c8ec9d5219c7c5739e22
adoumasseo <adoumasseo@gmail.com>
2024-11-15 13:13:54 +0100

    First Task Using asyncio

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''First module using asyncio'''
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''Here we use asyncio to simulate a wait'''
+    randValue = float(random.uniform(0, max_delay))
+    await asyncio.sleep(randValue)
+    return randValue
