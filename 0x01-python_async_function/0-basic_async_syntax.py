e813f0a07400d8a957ba7b25c920e8377fcb8f7e
adoumasseo <adoumasseo@gmail.com>
2024-11-15 13:15:58 +0100

    fix: Add annotation on randValue

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Here we use asyncio to simulate a wait'''
-    randValue = float(random.uniform(0, max_delay))
+    randValue: float = random.uniform(0, max_delay)
     await asyncio.sleep(randValue)
     return randValue
