f1e425a203f35aca30af98d56433ad4917bec28a
adoumasseo <adoumasseo@gmail.com>
2024-11-16 12:09:48 +0100

    Fix: Use Generator Instead of AsyncGenerator for the annotations Since we working with an older version of python

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-'''first taks module'''
+'''Task 0's module.
+'''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    '''Yield 10 times random float'''
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
